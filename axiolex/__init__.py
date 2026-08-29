25bcaacc72255d9d7cb310e1773a49457160bfff
vrraj <61334130+vrraj@users.noreply.github.com>
2026-08-28 17:15:54 -0700

    Bump version to 1.0.9 for TestPyPI release

--- axiolex/__init__.py
+++ axiolex/__init__.py
@@ -38,7 +38,7 @@ try:
     from importlib.metadata import version as _pkg_version
     __version__ = _pkg_version("axiolex")
 except Exception:
-    __version__ = "1.0.8"
+    __version__ = "1.0.9"
 
 __all__ = [
     "Axiolex",
